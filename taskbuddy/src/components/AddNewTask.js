44263a45e914bdbb272698af932f5b0a99a6e9f5
theblessed-coder <blessingnwakwuo8@gmail.com>
2023-11-02 02:04:03 +0100

    feat: Add AddNewTask component with modal functionality

--- taskbuddy/src/components/AddNewTask.js
+++ taskbuddy/src/components/AddNewTask.js
@@ -1,9 +1,22 @@
-import React from 'react'
+import React, { useState } from "react";
+import Modal from "./Modal";
 
 function AddNewTask() {
+  const [showModal, setShowModal] = useState(false);
+
   return (
-    <div>AddNewTask</div>
-  )
+    <div className="AddNewTask">
+      <div className="btn">
+        <button onClick={() => setShowModal(true)}>+ New Todo</button>
+      </div>
+      <Modal showModal={showModal} setShowModal={setShowModal}>
+        <div>
+          Hello World
+          <button onClick={() => setShowModal(false)}>hide</button>
+        </div>
+      </Modal>
+    </div>
+  );
 }
 
-export default AddNewTask
+export default AddNewTask;
--- taskbuddy/src/components/User.js
+++ taskbuddy/src/components/User.js
@@ -1,11 +1,11 @@
 import React from 'react'
-import log from '../images/log.png'
+import logo from '../images/logo.png'
 
 function User() {
   return (
     <div className='User'>
       <div className='logo'>
-        <img src={log} alt='logo' />
+        <img src={logo} alt='logo' />
       </div>
       <div className='info'>
         <p>Blessing Nwakwuo</p>
